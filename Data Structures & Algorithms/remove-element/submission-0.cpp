e8b751850d34ebcb4cd0bf730475b681ce38d9e5
Abhinandan132 <118281042+Abhinandan132@users.noreply.github.com>
2026-09-07 09:27:09 +0530

    Add: remove-element - submission-0

---
+++ Data Structures & Algorithms/remove-element/submission-0.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        // Naive approach, not doing it in place, create a separate vector, push non vals. 
+        // T.C O(n), S.C: O(n); 
+        vector<int> temp;
+        for(int i = 0; i < nums.size(); i++){
+            if (nums[i] != val)
+                temp.push_back(nums[i]); 
+        }
+        nums = temp; 
+        return temp.size(); 
+    }
+};
